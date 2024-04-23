619a5b42293a32e76e17ad8bd4d927d00222f03d
lha901222 <lha901222@gmail.com>
2024-04-23 10:11:14 +0900

    docs : 리액트 라우터

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./components/Layout";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
 
 function App() {
   return (
@@ -15,10 +17,14 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/profiles/:username" element={<Profile />} />
       </Route>
+
       <Route path="/articles" element={<Articles />}>
         <Route index element={<Navigate replace to="1" />} />
         <Route path=":id" element={<Article />} />
       </Route>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<Mypage />} />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
