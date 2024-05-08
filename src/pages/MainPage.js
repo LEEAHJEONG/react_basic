ac9077fa471accccd697156c662f56d35bb2e542
lha901222 <lha901222>
2024-05-08 10:43:19 +0900

    docs : hooks 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,39 +1,17 @@
-import React from "react";
-import Counter from "../components/Counter";
-import CounterReducer from "../components/CounterReducer";
-import InputReducer from "../components/InputReducer";
-import Average from "../components/Average";
+import React, { useState } from "react";
 
-const MainPage = () => {
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
   return (
     <div>
-      <h1>Hooks study</h1>
-      {/* <div>
-        <h2>useState</h2>
-        <h3>카운터앱</h3>
-        <Counter />
-      </div>
-      <div>
-        <h2>useEffecte</h2>
-        <h3>카운터앱</h3>
-        <Counter />
-      </div>
-      <div>
-        <h2>useReducer</h2>
-        <h3>예시: 리듀서 활용 카운터 앱</h3>
-        <CounterReducer />
-      </div>
-      <div>
-        <h3>예시: input 상태 관리하기</h3>
-        <InputReducer />
-      </div> */}
-      <div>
-        <h2>useRef</h2>
-        <h3>예시: 평균값 계산</h3>
-        <Average />
-      </div>
+      <p>
+        현재 카운터 값은 <b>{value} 입니다.</b>
+      </p>
+      <button onClick={() => setValue(value + 1)}>1 증가</button>
+      <button onClick={() => setValue(value - 1)}>1 감소</button>
     </div>
   );
 };
 
-export default MainPage;
+export default Counter;
